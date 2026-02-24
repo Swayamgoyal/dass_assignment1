7cb8517932d39ca824e4597be4a19dd709661f50
Swayamgoyal <swayamgoyal2005@gmail.com>
2026-02-24 15:02:27 +0530

    final

--- install-backend.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo Installing backend dependencies...
-cd /d "%~dp0backend"
-call npm install
-echo.
-echo Backend dependencies installed!
-echo.
-pause
--- install-frontend.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo Installing frontend dependencies...
-cd /d "%~dp0frontend"
-call npm install
-echo.
-echo Frontend dependencies installed!
-echo.
-pause
